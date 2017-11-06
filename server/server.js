4c8ca545bc3935eb76a7c0b75c5954de57d2357b
meng zhou <mengzhou44@gmail.com>
2017-11-06 16:35:55 -0700

    add get todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { User } = require('./models/user');
 const { ToDo } = require('./models/todo');
@@ -27,6 +28,25 @@ app.get('/todos', (req, res) => {
   ToDo.find()
     .then(todos => {
       res.status(200).send(todos);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  ToDo.findById(req.params.id)
+    .then(todo => {
+      if (!todo) {
+        res.status(400).send('To do record is not found!');
+      }
+      res.status(200).send(todo);
     })
     .catch(err => {
       res.status(400).send(err);

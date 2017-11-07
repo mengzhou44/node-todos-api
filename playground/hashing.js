35855c35e6b78ed5b2d280901a8a58f380063b65
meng zhou <mengzhou44@gmail.com>
2017-11-07 16:00:42 -0700

    hash password before saving it

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,19 @@
 const { SHA256 } = require('crypto-js');
+const bcrypt = require('bcryptjs');
 
+var password = '1234!';
+/* bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+}); */
+
+const hashed = '$2a$10$sGsCV5Ru.1/7Yt8YN4miEeVXyGZDZ6B6ZG2ufiPmKjNWQDdhrtdui';
+
+bcrypt.compare(password, hashed, (err, result) => {
+  console.log('result ' + result);
+});
+/* 
 const message = 'Hi, I am a message!';
 const hash = SHA256(message).toString();
 
@@ -23,4 +37,4 @@ if (resultHash === token.hash) {
   console.log('Data is not modified!');
 } else {
   console.log('Warning! Data is  modified!');
-}
+} */

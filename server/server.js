65cbc8f3b81451c8fd3bd34b1e646f705ba4f6fa
meng zhou <mengzhou44@gmail.com>
2017-11-06 17:04:43 -0700

    add test for get todos/:id

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get('/todos/:id', (req, res) => {
   ToDo.findById(req.params.id)
     .then(todo => {
       if (!todo) {
-        res.status(400).send('To do record is not found!');
+        res.status(404).send();
       }
       res.status(200).send(todo);
     })
